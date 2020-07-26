4b107cfc6e563ed846aaa5f55f822543c0107199
Henry Bley-Vroman <olets@olets.dev>
2020-07-26 15:50:58 -0400

    test(all): reduce reliance on other abbr commands

--- tests/tests.zsh
+++ tests/tests.zsh
@@ -10,20 +10,8 @@ typeset -a result
 test_abbr_abbreviation="zsh_abbr_test"
 test_abbr_expansion="zsh abbr test"
 
-# Can add an abbreviation with the add flag
-message="abbr add"
-abbr add $test_abbr_abbreviation=$test_abbr_expansion
-if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$test_abbr_abbreviation]} == $test_abbr_expansion ]]; then
-	message="$fg[green]PASS$reset_color $message"
-else
-	message="$fg[red]FAIL$reset_color $message"
-	pass=1
-fi
-echo $message
-echo
-
 # Can erase an abbreviation
-message="abbr add and erase "
+message="abbr erase (dependency: add)"
 abbr add $test_abbr_abbreviation=$test_abbr_expansion
 abbr erase $test_abbr_abbreviation
 if [[ ${#ABBR_REGULAR_USER_ABBREVIATIONS} == 0 ]]; then
@@ -35,11 +23,62 @@ fi
 echo $message
 echo
 
+# Can add an abbreviation with the add flag
+message="abbr add (dependency: erase)"
+abbr add $test_abbr_abbreviation=$test_abbr_expansion
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$test_abbr_abbreviation]} == $test_abbr_expansion ]]; then
+	message="$fg[green]PASS$reset_color $message"
+else
+	message="$fg[red]FAIL$reset_color $message"
+	pass=1
+fi
+echo $message
+abbr erase $test_abbr_abbreviation
+echo
+
+# Can add a global abbreviation with the add flag
+message="abbr add -g (dependency: erase)"
+abbr add -g $test_abbr_abbreviation=$test_abbr_expansion
+if [[ ${(Q)ABBR_GLOBAL_USER_ABBREVIATIONS[$test_abbr_abbreviation]} == $test_abbr_expansion ]]; then
+	message="$fg[green]PASS$reset_color $message"
+else
+	message="$fg[red]FAIL$reset_color $message"
+	pass=1
+fi
+echo $message
+abbr erase $test_abbr_abbreviation
+echo
+
+# Can add a regular session abbreviation with the add flag
+message="abbr add -S (dependency: erase)"
+abbr add -S $test_abbr_abbreviation=$test_abbr_expansion
+if [[ ${(Q)ABBR_REGULAR_SESSION_ABBREVIATIONS[$test_abbr_abbreviation]} == $test_abbr_expansion ]]; then
+	message="$fg[green]PASS$reset_color $message"
+else
+	message="$fg[red]FAIL$reset_color $message"
+	pass=1
+fi
+echo $message
+abbr erase $test_abbr_abbreviation
+echo
+
+# Can add a global session abbreviation with the add flag
+message="abbr add -S -g (dependency: erase)"
+abbr add -S -g $test_abbr_abbreviation=$test_abbr_expansion
+if [[ ${(Q)ABBR_GLOBAL_SESSION_ABBREVIATIONS[$test_abbr_abbreviation]} == $test_abbr_expansion ]]; then
+	message="$fg[green]PASS$reset_color $message"
+else
+	message="$fg[red]FAIL$reset_color $message"
+	pass=1
+fi
+echo $message
+abbr erase $test_abbr_abbreviation
+echo
+
 # Can add an abbreviation without the add flag
-message="abbr "
+message="abbr (dependency: erase)"
 abbr $test_abbr_abbreviation=$test_abbr_expansion
-result=( ${(f)"$(abbr list-commands)"} )
-if [[ $result[1] == "abbr $test_abbr_abbreviation=${(qqq)test_abbr_expansion}" ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$test_abbr_abbreviation]} == $test_abbr_expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -50,11 +89,10 @@ abbr erase $test_abbr_abbreviation
 echo
 
 # Can clear session abbreviations
-message="abbr clear-session "
+message="abbr clear-session (add session dependency)"
 abbr -S $test_abbr_abbreviation=$test_abbr_expansion
 abbr clear-session
-result=( ${(f)"$(abbr list-commands)"} )
-if [[ ${#result} == 0 ]]; then
+if [[ ${#ABBR_REGULAR_SESSION_ABBREVIATIONS} == 0 ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -64,10 +102,9 @@ echo $message
 echo
 
 # Can expand an abbreviation in a script
-message="abbr expand "
+message="abbr expand (dependency: erase)"
 abbr $test_abbr_abbreviation=$test_abbr_expansion
-result=( ${(f)"$(abbr expand $test_abbr_abbreviation)"} )
-if [[ $result[1] == $test_abbr_expansion ]]; then
+if [[ $(abbr expand $test_abbr_abbreviation) == $test_abbr_expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -78,11 +115,12 @@ abbr erase $test_abbr_abbreviation
 echo
 
 # # Can rename an abbreviation
-message="abbr rename "
+message="abbr rename (dependency: erase)"
 abbr $test_abbr_abbreviation=$test_abbr_expansion
 abbr rename $test_abbr_abbreviation ${test_abbr_abbreviation}_new
-result=( ${(f)"$(abbr expand ${test_abbr_abbreviation}_new)"} )
-if [[ $result[1] == $test_abbr_expansion ]]; then
+if ! (( ${+ABBR_REGULAR_USER_ABBREVIATIONS[$test_abbr_abbreviation]} )) && \
+	[[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[${test_abbr_abbreviation}_new]} == $test_abbr_expansion ]]; then
+
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -95,10 +133,9 @@ echo
 # Double-quoted single quotes are preserved
 abbreviation=a
 expansion="b'c'd"
-message="abbr a=$expansion "
+message="abbr a=$expansion (dependency: erase)"
 abbr $abbreviation=$expansion
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -111,10 +148,9 @@ echo
 # Single-quoted double quotes are preserved
 abbreviation=a
 expansion='b"c"d'
-message="abbr a=$expansion "
+message="abbr a=$expansion (dependency: erase)"
 abbr $abbreviation=$expansion
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -127,10 +163,9 @@ echo
 # Bare single quotes at the start of a string are swallowed
 abbreviation=a
 expansion='b'cd
-message="abbr a='b'cd "
+message="abbr a='b'cd (dependency: erase)"
 abbr $abbreviation=$expansion
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -143,10 +178,9 @@ echo
 # Bare single quotes in the middle of a string are swallowed
 abbreviation=a
 expansion=b'c'd
-message="abbr a=b'c'd "
+message="abbr a=b'c'd (dependency: erase)"
 abbr $abbreviation=$expansion
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -159,10 +193,9 @@ echo
 # Bare double quotes at the start of a string are swallowed
 abbreviation=a
 expansion="b"cd
-message="abbr a=\"b\"cd "
+message="abbr a=\"b\"cd (dependency: erase)"
 abbr $abbreviation=$expansion
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -175,10 +208,9 @@ echo
 # Bare double quotes in the middle of a string are swallowed
 abbreviation=a
 expansion=b"c"d
-message="abbr a=b\"c\"d "
+message="abbr a=b\"c\"d (dependency: erase)"
 abbr $abbreviation=$expansion
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -191,11 +223,10 @@ echo
 # Can import aliases
 abbreviation=zsh_abbr_test_alias
 expansion=abc
-message="importing a single-word alias "
+message="importing a single-word alias (dependency: erase)"
 alias $abbreviation=$expansion
 abbr import-aliases
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -212,11 +243,10 @@ source ${0:A:h}/../zsh-abbr.zsh
 # Can import a multi-word alias
 abbreviation=zsh_abbr_test_alias
 expansion="a b"
-message="importing a multi-word alias "
+message="importing a multi-word alias (dependency: erase)"
 alias $abbreviation=$expansion
 abbr import-aliases
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -233,11 +263,10 @@ source ${0:A:h}/../zsh-abbr.zsh
 # Can import a double-quoted alias with escaped double quotation marks
 abbreviation=zsh_abbr_test_alias
 expansion="a \"b\""
-message="importing a double-quoted multi-word alias with escape double quotes "
+message="importing a double-quoted multi-word alias dependency: escape double quotes (has erase)"
 alias $abbreviation=$expansion
 abbr import-aliases
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -254,11 +283,10 @@ source ${0:A:h}/../zsh-abbr.zsh
 # Can import a single-quoted alias with double quotation marks
 abbreviation=zsh_abbr_test_alias
 expansion='a "b"'
-message="importing a single-quoted multi-word alias with double quotes "
+message="importing a single-quoted multi-word alias dependency: double quotes (has erase)"
 alias $abbreviation=$expansion
 abbr import-aliases
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -275,11 +303,10 @@ source ${0:A:h}/../zsh-abbr.zsh
 # Can import a double-quoted alias with single quotation marks
 abbreviation=zsh_abbr_test_alias
 expansion="a 'b'"
-message="importing a double-quoted multi-word alias with single quotes "
+message="importing a double-quoted multi-word alias dependency: single quotes (has erase)"
 alias $abbreviation=$expansion
 abbr import-aliases
-result=( ${(f)"$(abbr expand $abbreviation)"} )
-if [[ $result[1] == $expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$abbreviation]} == $expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
@@ -294,8 +321,7 @@ unalias $abbreviation
 message="abbreviation deleted externally cannot be expanded"
 abbr add $test_abbr_abbreviation=$test_abbr_expansion
 echo '' > $ABBR_USER_ABBREVIATIONS_FILE
-result=( ${(f)"$(abbr expand $test_abbr_abbreviation)"} )
-if [[ ${#result} == 0 ]]; then
+if [[ -z $(abbr expand $test_abbr_abbreviation) ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	echo $result[1]
@@ -306,10 +332,9 @@ echo $message
 echo
 
 # Can add a user abbreviation from outside abbr without data loss
-message="abbreviation added externally can be expanded"
+message="abbreviation added externally can be expanded (dependency: erase)"
 echo "abbr add $test_abbr_abbreviation='$test_abbr_expansion'" > $ABBR_USER_ABBREVIATIONS_FILE
-result=( ${(f)"$(abbr expand $test_abbr_abbreviation)"} )
-if [[ $result[1] == $test_abbr_expansion ]]; then
+if [[ ${(Q)ABBR_REGULAR_USER_ABBREVIATIONS[$test_abbr_abbreviation]} == $test_abbr_expansion ]]; then
 	message="$fg[green]PASS$reset_color $message"
 else
 	message="$fg[red]FAIL$reset_color $message"
