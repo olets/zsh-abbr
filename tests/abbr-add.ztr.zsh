e87da529f7d51b5344084f8a2b4ee79dc935dfef
Henry Bley-Vroman <olets@olets.dev>
2023-03-18 13:53:40 -1000

    tests(add): rely on expand

--- tests/abbr-add.ztr.zsh
+++ tests/abbr-add.ztr.zsh
@@ -1,15 +1,15 @@
 #!/usr/bin/env zsh
 
 abbr add $test_abbr_abbreviation=$test_abbr_expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr expand $test_abbr_abbreviation) == $test_abbr_expansion ]]' \
 	"Can add an abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation
 
 abbr add -S $test_abbr_abbreviation=$test_abbr_expansion
-ztr test '[[ ${ABBR_REGULAR_SESSION_ABBREVIATIONS[${(qqq)test_abbr_abbreviation}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr -S expand $test_abbr_abbreviation) == $test_abbr_expansion ]]' \
 	"Can add a regular session abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation
 
 # Manual
@@ -24,57 +24,57 @@ abbr erase $test_abbr_abbreviation
 # Implicit
 
 abbr $test_abbr_abbreviation=$test_abbr_expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr expand $test_abbr_abbreviation) == $test_abbr_expansion ]]' \
 	"Can add an abbreviation without the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation
 
 abbr $test_abbr_abbreviation_multiword=$test_abbr_expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_multiword}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr expand $test_abbr_abbreviation_multiword) == $test_abbr_expansion ]]' \
 	"Can add a multi-word abbreviation without the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation_multiword
 
 # Global
 
 abbr add -g $test_abbr_abbreviation=$test_abbr_expansion
-ztr test '[[ ${ABBR_GLOBAL_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr -g expand $test_abbr_abbreviation) == $test_abbr_expansion ]]' \
 	"Can add a global abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation
 
 abbr add -S -g $test_abbr_abbreviation=$test_abbr_expansion
-ztr test '[[ ${ABBR_GLOBAL_SESSION_ABBREVIATIONS[${(qqq)test_abbr_abbreviation}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr -g -S expand $test_abbr_abbreviation) == $test_abbr_expansion ]]' \
 	"Can add a global session abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation
 
 # Multiword
 
 abbr add $test_abbr_abbreviation_multiword=$test_abbr_expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_multiword}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr expand $test_abbr_abbreviation_multiword) == $test_abbr_expansion ]]' \
 	"Can add a multi-word abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation_multiword
 
 abbr add -S $test_abbr_abbreviation_multiword=$test_abbr_expansion
-ztr test '[[ ${ABBR_REGULAR_SESSION_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_multiword}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr -S expand $test_abbr_abbreviation_multiword) == $test_abbr_expansion ]]' \
 	"Can add a multi-word regular session abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation_multiword
 
 # Multiword global
 
 abbr add -g $test_abbr_abbreviation_multiword=$test_abbr_expansion
-ztr test '[[ ${ABBR_GLOBAL_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_multiword}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr -g expand $test_abbr_abbreviation_multiword) == $test_abbr_expansion ]]' \
 	"Can add a multi-word global abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation_multiword
 
 abbr add -S -g $test_abbr_abbreviation_multiword=$test_abbr_expansion
-ztr test '[[ ${ABBR_GLOBAL_SESSION_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_multiword}]} == ${(qqq)test_abbr_expansion} ]]' \
+ztr test '[[ $(abbr -g -S expand $test_abbr_abbreviation_multiword) == $test_abbr_expansion ]]' \
 	"Can add a multi-word global session abbreviation with the add subcommand" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $test_abbr_abbreviation_multiword
 
 # Quotes
@@ -82,47 +82,47 @@ abbr erase $test_abbr_abbreviation_multiword
 abbreviation=a
 expansion='b'cd
 abbr $abbreviation=$expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)abbreviation}]} == ${(qqq)expansion} ]]' \
+ztr test '[[ $(abbr expand $abbreviation) == $expansion ]]' \
 	"Bare single quotes at the start of the expansion are swallowed" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $abbreviation
 
 abbreviation=a
 expansion=b'c'd
 abbr $abbreviation=$expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)abbreviation}]} == ${(qqq)expansion} ]]' \
+ztr test '[[ $(abbr expand $abbreviation) == $expansion ]]' \
 	"Bare single quotes in the middle of the expansion are swallowed" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $abbreviation
 
 abbreviation=a
 expansion='b"c"d'
 abbr $abbreviation=$expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)abbreviation}]} == ${(qqq)expansion} ]]' \
+ztr test '[[ $(abbr expand $abbreviation) == $expansion ]]' \
 	"Single-quoted double quotes in the expansion are preserved" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $abbreviation
 
 abbreviation=a
 expansion="b"cd
 abbr $abbreviation=$expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)abbreviation}]} == ${(qqq)expansion} ]]' \
+ztr test '[[ $(abbr expand $abbreviation) == $expansion ]]' \
 	"Bare double quotes at the start of the expansion are swallowed" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $abbreviation
 
 abbreviation=a
 expansion=b"c"d
 abbr $abbreviation=$expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)abbreviation}]} == ${(qqq)expansion} ]]' \
+ztr test '[[ $(abbr expand $abbreviation) == $expansion ]]' \
 	"Bare double quotes in the middle of the expansion are swallowed" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $abbreviation
 
 abbreviation=a
 expansion="b'c'd"
 abbr $abbreviation=$expansion
-ztr test '[[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)abbreviation}]} == ${(qqq)expansion} ]]' \
+ztr test '[[ $(abbr expand $abbreviation) == $expansion ]]' \
 	"Double-quoted single quotes in the expansion are preserved" \
-	"Dependencies: erase"
+	"Dependencies: erase, expand"
 abbr erase $abbreviation
