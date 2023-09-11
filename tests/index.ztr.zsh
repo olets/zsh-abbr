8c572b2edc211d9eb13a2de6e02e593ac672e8df
Henry Bley-Vroman <olets@olets.dev>
2023-09-10 22:40:26 -1000

    style(tests): declare variables before assigning

--- tests/index.ztr.zsh
+++ tests/index.ztr.zsh
@@ -17,25 +17,34 @@
 # ```
 
 main() {
-	local cmd
+	local \
+		abbr_dir \
+		abbr_quiet_saved \
+		abbr_user_abbreviations_file_saved \
+		cmd \
+		test_abbr_abbreviation \
+		test_abbr_abbreviation_2 \
+		test_abbr_abbreviation_multiword \
+		test_abbr_abbreviation_multiword_2 \
+		test_abbr_expansion \
+		test_abbr_expansion_2 \
+		test_dir
 
-	cmd=$1
-	
 	ztr_path=${ztr_path:-$ZTR_PATH}
 
 	if [[ -z $ztr_path ]]; then
 		printf "You must provide \$ztr_path\n"
 		return 1
 	fi
-	
-	. $ztr_path
+
+	cmd=$1
+
+	abbr_dir=${0:A:h}
+	test_dir=$abbr_dir/tests
 
 	# Save user configuration
-	local abbr_quiet_saved=$ABBR_QUIET
-	local abbr_user_abbreviations_file_saved=$ABBR_USER_ABBREVIATIONS_FILE
-
-	local abbr_dir=${0:A:h}
-	local test_dir=$abbr_dir/tests
+	abbr_quiet_saved=$ABBR_QUIET
+	abbr_user_abbreviations_file_saved=$ABBR_USER_ABBREVIATIONS_FILE
 
 	# Configure
 	ABBR_QUIET=1
@@ -43,15 +52,18 @@ main() {
 
 	# Set up data
 	touch $ABBR_USER_ABBREVIATIONS_FILE
-	local test_abbr_abbreviation="zsh_abbr_test"
-	local test_abbr_abbreviation_2="zsh_abbr_test_2"
-	local test_abbr_abbreviation_multiword="zsh_abbr_test second_word"
-	local test_abbr_abbreviation_multiword_2="zsh_abbr_test other_second_word"
-	local test_abbr_expansion="zsh abbr test"
-	local test_abbr_expansion_2="zsh abbr test 2"
+	test_abbr_abbreviation="zsh_abbr_test"
+	test_abbr_abbreviation_2="zsh_abbr_test_2"
+	test_abbr_abbreviation_multiword="zsh_abbr_test second_word"
+	test_abbr_abbreviation_multiword_2="zsh_abbr_test other_second_word"
+	test_abbr_expansion="zsh abbr test"
+	test_abbr_expansion_2="zsh abbr test 2"
 
+	# Source dependencies
 	. $abbr_dir/zsh-abbr.zsh
-	. $ZTR_PATH
+	. $ztr_path
+
+	# Clear zsh-test-runner summary
 	ztr clear
 
 	# Run tests
@@ -65,16 +77,16 @@ main() {
 	fi
 
 	# Remove artifacts
-
-	if $('builtin' 'command' -v abbr); then
-		abbr load
-	fi
 	rm -f $ABBR_USER_ABBREVIATIONS_FILE
 
 	# Reset
 	ABBR_QUIET=$abbr_quiet_saved
 	ABBR_USER_ABBREVIATIONS_FILE=$abbr_user_abbreviations_file_saved
+	if $(command -v _abbr_load_user_abbreviations); then
+		_abbr_load_user_abbreviations
+	fi
 
+	# Print test suite results
 	echo
 	ztr summary
 }
