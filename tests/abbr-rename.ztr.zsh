0eca5afd5bf2cb65817ccce6d0309dca4278c400
Henry Bley-Vroman <olets@olets.dev>
2023-09-15 10:23:02 -1000

    tests(rename): more thorough reset

--- tests/abbr-rename.ztr.zsh
+++ tests/abbr-rename.ztr.zsh
@@ -2,13 +2,14 @@
 
 main() {
 	emulate -LR zsh
-	
+
 	abbr $test_abbr_abbreviation=$test_abbr_expansion
 	abbr rename $test_abbr_abbreviation $test_abbr_abbreviation_2
 	ztr test '! (( ${+ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation}]} )) \
 			&& [[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_2}]} == ${(qqq)test_abbr_expansion} ]]' \
 		"Can rename a single-word abbreviation to another single word" \
 		"Dependencies: erase"
+	abbr erase --quiet $test_abbr_abbreviation
 	abbr erase $test_abbr_abbreviation_2
 
 	abbr $test_abbr_abbreviation=$test_abbr_expansion
@@ -17,6 +18,7 @@ main() {
 			&& [[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_multiword}]} == ${(qqq)test_abbr_expansion} ]]' \
 		"Can rename an single-word abbreviation to multiple words" \
 		"Dependencies: erase"
+	abbr erase --quiet $test_abbr_abbreviation
 	abbr erase $test_abbr_abbreviation_multiword
 
 	abbr $test_abbr_abbreviation_multiword=$test_abbr_expansion
@@ -25,6 +27,7 @@ main() {
 			&& [[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation}]} == ${(qqq)test_abbr_expansion} ]]' \
 		"Can rename an multi-word abbreviation to a single word" \
 		"Dependencies: erase"
+	abbr erase --quiet $test_abbr_abbreviation_multiword
 	abbr erase $test_abbr_abbreviation
 
 	abbr $test_abbr_abbreviation_multiword=$test_abbr_expansion
@@ -33,6 +36,7 @@ main() {
 			&& [[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_multiword_2}]} == ${(qqq)test_abbr_expansion} ]]' \
 		"Can rename an multi-word abbreviation to different words" \
 		"Dependencies: erase"
+	abbr erase --quiet $test_abbr_abbreviation_multiword
 	abbr erase $test_abbr_abbreviation_multiword_2
 
 	abbr $test_abbr_abbreviation=$test_abbr_expansion
@@ -42,6 +46,7 @@ main() {
 			&& [[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_2}]} == ${(qqq)test_abbr_expansion_2} ]]' \
 		"Cannot rename a single-word abbreviation to an existing abbreviation" \
 		"Dependencies: erase"
+	abbr erase --quiet $test_abbr_abbreviation
 	abbr erase $test_abbr_abbreviation_2
 
 	abbr $test_abbr_abbreviation=$test_abbr_expansion
@@ -51,6 +56,7 @@ main() {
 			&& [[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_2}]} == ${(qqq)test_abbr_expansion} ]]' \
 		"Can rename a single-word abbreviation to an existing abbreviation with --force" \
 		"Dependencies: erase"
+	abbr erase --quiet $test_abbr_abbreviation
 	abbr erase $test_abbr_abbreviation_2
 
 	abbr $test_abbr_abbreviation=$test_abbr_expansion
@@ -60,6 +66,7 @@ main() {
 			&& [[ ${ABBR_REGULAR_USER_ABBREVIATIONS[${(qqq)test_abbr_abbreviation_2}]} == ${(qqq)test_abbr_expansion} ]]' \
 		"Can rename a single-word abbreviation to an existing abbreviation with -f" \
 		"Dependencies: erase"
+	abbr erase --quiet $test_abbr_abbreviation
 	abbr erase $test_abbr_abbreviation_2
 }
 
