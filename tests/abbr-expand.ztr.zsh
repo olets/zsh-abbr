51c04cf5070d201b6f68369015321e377e857679
Henry Bley-Vroman <olets@olets.dev>
2024-05-13 08:35:15 -1000

    feat(_abbr_regular_expansion): support ignored prefixes

--- tests/abbr-expand.ztr.zsh
+++ tests/abbr-expand.ztr.zsh
@@ -30,6 +30,16 @@ main() {
 			"Can expand a global session abbreviation in a script" \
 			"Dependencies: erase"
 
+		abbr add $test_abbr_abbreviation=$test_abbr_expansion
+		ztr test '[[ $(abbr expand "$ABBR_IGNORED_ABBREVIATION_PREFIXES[1] $test_abbr_abbreviation") == "$ABBR_IGNORED_ABBREVIATION_PREFIXES[1] $test_abbr_expansion" ]]' \
+			"Can expand an prefixed abbreviation in a script" \
+			"Dependencies: erase"
+
+		abbr add -S $test_abbr_abbreviation=$test_abbr_expansion
+		ztr test '[[ $(abbr expand "$ABBR_IGNORED_ABBREVIATION_PREFIXES[1] $test_abbr_abbreviation") == "$ABBR_IGNORED_ABBREVIATION_PREFIXES[1] $test_abbr_expansion" ]]' \
+			"Can expand a prefixed session abbreviation in a script" \
+			"Dependencies: erase"
+
 		abbr $test_abbr_abbreviation_multiword=$test_abbr_expansion
 		ztr test '[[ $(abbr expand $test_abbr_abbreviation_multiword) == $test_abbr_expansion ]]' \
 			"Can expand a two-word abbreviation in a script" \
