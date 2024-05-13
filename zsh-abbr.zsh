ef3d935c17380ea48cf6b68c006ce33e9a56b17f
Henry Bley-Vroman <olets@olets.dev>
2024-05-13 11:59:10 -1000

    feat(_abbr_regular_expansion): prefixes don't require space delimiter

--- zsh-abbr.zsh
+++ zsh-abbr.zsh
@@ -62,7 +62,7 @@ if [[ -z $ABBR_USER_ABBREVIATIONS_FILE ]]; then
 fi
 
 if ! [[ -n ${(t)ABBR_REGULAR_ABBREVIATION_PREFIXES} ]]; then
-  ABBR_REGULAR_ABBREVIATION_PREFIXES=( sudo )
+  ABBR_REGULAR_ABBREVIATION_PREFIXES=( 'sudo ' )
 fi
 typeset -ga ABBR_REGULAR_ABBREVIATION_PREFIXES
 
@@ -1081,7 +1081,7 @@ _abbr_regular_expansion() {
         prefix=$prefixes[1]
         shift prefixes
 
-        abbreviation_sans_prefix="${abbreviation#$prefix }"
+        abbreviation_sans_prefix="${abbreviation#$prefix}"
 
         if (( session )); then
           expansion=$ABBR_REGULAR_SESSION_ABBREVIATIONS[${(qqq)abbreviation_sans_prefix}]
@@ -1094,7 +1094,7 @@ _abbr_regular_expansion() {
         fi
 
         if [[ $abbreviation_sans_prefix != $abbreviation ]]; then
-          expansion="${(qqq)prefix} $expansion"
+          expansion="${(qqq)prefix}$expansion"
         fi
       done
 
