752e9fcc4daff680545c30f8f857913d66f6f5e6
Henry Bley-Vroman <olets@olets.dev>
2024-06-15 17:13:25 -1000

    feat(abbr-expand-and-space): deprecation warning points to abbr-expand-and-insert

--- zsh-abbr.zsh
+++ zsh-abbr.zsh
@@ -1507,7 +1507,7 @@ _abbr_init() {
         }
 
         abbr-expand-and-space() {
-          _abbr_warn_deprecation abbr-expand-and-space
+          _abbr_warn_deprecation abbr-expand-and-space abbr-expand-and-insert
           abbr-expand-and-insert
         }
         # END Deprecation notices for functions
