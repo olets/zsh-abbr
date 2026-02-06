b6ec27caa5164543b7b5876880e68ff2c699fd33
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2026-02-06 12:42:56 -1000

    chore(abbr-expand-tests): restore indentation

--- tests/abbr-expand-line.ztr.zsh
+++ tests/abbr-expand-line.ztr.zsh
@@ -16,92 +16,92 @@ main() {
     abbr-expand-line foo
     res=$?
     ztr test '[[ -z $reply[abbreviation] ]] \
-    && [[ $reply[expansion_cursor_set] == 0 ]] \
-    && [[ -z $reply[expansion] ]] \
-    && [[ $reply[linput] == foo ]] \
-    && [[ $reply[loutput] == foo ]] \
-    && [[ -z $reply[rinput] ]] \
-    && [[ -z $reply[routput] ]] \
-    && [[ -z $reply[type] ]] \
-    && (( res == 1 ))
-    ' \
-    "abbr-expand-line with no abbreviation, no expansion cursor: falsy, output text matches input text, no expansion data"
+        && [[ $reply[expansion_cursor_set] == 0 ]] \
+        && [[ -z $reply[expansion] ]] \
+        && [[ $reply[linput] == foo ]] \
+        && [[ $reply[loutput] == foo ]] \
+        && [[ -z $reply[rinput] ]] \
+        && [[ -z $reply[routput] ]] \
+        && [[ -z $reply[type] ]] \
+        && (( res == 1 ))
+        ' \
+      "abbr-expand-line with no abbreviation, no expansion cursor: falsy, output text matches input text, no expansion data"
 
     abbr-expand-line "$ABBR_EXPANSION_CURSOR_MARKER foo"
     res=$?
     ztr test '[[ -z $reply[abbreviation] ]] \
-    && [[ $reply[expansion_cursor_set] == 0 ]] \
-    && [[ -z $reply[expansion] ]] \
-    && [[ $reply[linput] == "$ABBR_EXPANSION_CURSOR_MARKER foo" ]] \
-    && [[ $reply[loutput] == "$ABBR_EXPANSION_CURSOR_MARKER foo" ]] \
-    && [[ -z $reply[rinput] ]] \
-    && [[ -z $reply[routput] ]] \
-    && [[ -z $reply[type] ]] \
-    && (( res == 1 ))
-    ' \
-    "abbr-expand-line with no abbreviation, expansion cursor: falsy, output text matches input text, no expansion data"
+        && [[ $reply[expansion_cursor_set] == 0 ]] \
+        && [[ -z $reply[expansion] ]] \
+        && [[ $reply[linput] == "$ABBR_EXPANSION_CURSOR_MARKER foo" ]] \
+        && [[ $reply[loutput] == "$ABBR_EXPANSION_CURSOR_MARKER foo" ]] \
+        && [[ -z $reply[rinput] ]] \
+        && [[ -z $reply[routput] ]] \
+        && [[ -z $reply[type] ]] \
+        && (( res == 1 ))
+        ' \
+      "abbr-expand-line with no abbreviation, expansion cursor: falsy, output text matches input text, no expansion data"
 
     abbr $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line $test_abbr_abbreviation
     res=$?
     ztr test '[[ $reply[abbreviation] == $test_abbr_abbreviation ]] \
-    && [[ $reply[expansion_cursor_set] == 0 ]] \
-    && [[ $reply[expansion] == $test_abbr_expansion ]] \
-    && [[ $reply[linput] == $test_abbr_abbreviation ]] \
-    && [[ $reply[loutput] == $test_abbr_expansion ]] \
-    && [[ -z $reply[rinput] ]] \
-    && [[ -z $reply[routput] ]] \
-    && [[ $reply[type] == regular ]] \
-    && (( res == 0 ))
-    ' \
-    "abbr-expand-line returns correct values when expanding a line containing only a regular abbreviation"
+        && [[ $reply[expansion_cursor_set] == 0 ]] \
+        && [[ $reply[expansion] == $test_abbr_expansion ]] \
+        && [[ $reply[linput] == $test_abbr_abbreviation ]] \
+        && [[ $reply[loutput] == $test_abbr_expansion ]] \
+        && [[ -z $reply[rinput] ]] \
+        && [[ -z $reply[routput] ]] \
+        && [[ $reply[type] == regular ]] \
+        && (( res == 0 ))
+        ' \
+      "abbr-expand-line returns correct values when expanding a line containing only a regular abbreviation"
 
     abbr -g $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line $test_abbr_abbreviation
     res=$?
     ztr test '[[ $reply[abbreviation] == $test_abbr_abbreviation ]] \
-    && [[ $reply[expansion_cursor_set] == 0 ]] \
-    && [[ $reply[expansion] == $test_abbr_expansion ]] \
-    && [[ $reply[linput] == $test_abbr_abbreviation ]] \
-    && [[ $reply[loutput] == $test_abbr_expansion ]] \
-    && [[ -z $reply[rinput] ]] \
-    && [[ -z $reply[routput] ]] \
-    && [[ $reply[type] == global ]] \
-    && (( res == 0 ))
-    ' \
-    "abbr-expand-line returns correct values when expanding a line containing only a global abbreviation"
+        && [[ $reply[expansion_cursor_set] == 0 ]] \
+        && [[ $reply[expansion] == $test_abbr_expansion ]] \
+        && [[ $reply[linput] == $test_abbr_abbreviation ]] \
+        && [[ $reply[loutput] == $test_abbr_expansion ]] \
+        && [[ -z $reply[rinput] ]] \
+        && [[ -z $reply[routput] ]] \
+        && [[ $reply[type] == global ]] \
+        && (( res == 0 ))
+        ' \
+      "abbr-expand-line returns correct values when expanding a line containing only a global abbreviation"
 
     ztr test 'abbr-expand-line "" "abc"; \
-      [[ -z $reply[loutput] ]] && [[ $reply[routput] == "abc" ]]' \
+        [[ -z $reply[loutput] ]] && [[ $reply[routput] == "abc" ]]' \
       "abbr-expand-line supports empty LBUFFER"
 
     abbr $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line $test_abbr_abbreviation right_text
     res=$?
     ztr test '[[ $reply[abbreviation] == $test_abbr_abbreviation ]] \
-    && [[ $reply[rinput] == right_text ]] \
-    && (( res == 0 ))
-    ' \
-    "abbr-expand-line preserves right text when expanding a regular abbrevation"
+        && [[ $reply[rinput] == right_text ]] \
+        && (( res == 0 ))
+        ' \
+      "abbr-expand-line preserves right text when expanding a regular abbrevation"
 
     abbr -g $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line $test_abbr_abbreviation right_text
     res=$?
     ztr test '[[ $reply[abbreviation] == $test_abbr_abbreviation ]] \
-    && [[ $reply[rinput] == right_text ]] \
-    && (( res == 0 ))
-    ' \
-    "abbr-expand-line preserves right text when expanding a global abbrevation"
+        && [[ $reply[rinput] == right_text ]] \
+        && (( res == 0 ))
+        ' \
+      "abbr-expand-line preserves right text when expanding a global abbrevation"
 
     ABBR_SET_EXPANSION_CURSOR=1
     abbr $test_abbr_abbreviation=$test_abbr_expansion$ABBR_EXPANSION_CURSOR_MARKER$test_abbr_expansion_2
     abbr-expand-line $test_abbr_abbreviation
     res=$?
     ztr test '[[ $reply[loutput] == $test_abbr_expansion ]] \
-    && [[ $reply[routput] == $test_abbr_expansion_2 ]] \
-    && (( res == 0 ))
-    ' \
-    "abbr-expand-line can set the expansion cursor when expanding a regular abbreviation"
+        && [[ $reply[routput] == $test_abbr_expansion_2 ]] \
+        && (( res == 0 ))
+        ' \
+      "abbr-expand-line can set the expansion cursor when expanding a regular abbreviation"
     ABBR_SET_EXPANSION_CURSOR=0
 
     ABBR_SET_EXPANSION_CURSOR=1
@@ -109,29 +109,29 @@ main() {
     abbr-expand-line $test_abbr_abbreviation
     res=$?
     ztr test '[[ $reply[loutput] == $test_abbr_expansion ]] \
-    && [[ $reply[routput] == $test_abbr_expansion_2 ]] \
-    && (( res == 0 ))
-    ' \
-    "abbr-expand-line can set the expansion cursor when expanding a global abbreviation"
+        && [[ $reply[routput] == $test_abbr_expansion_2 ]] \
+        && (( res == 0 ))
+        ' \
+      "abbr-expand-line can set the expansion cursor when expanding a global abbreviation"
     ABBR_SET_EXPANSION_CURSOR=0
 
     abbr $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line "foo $test_abbr_abbreviation" right_text
 
     ztr test '[[ $reply[linput] == $reply[loutput] ]] \
-    && [[ $reply[rinput] == $reply[routput] ]] \
-    && ((res == 1 ))
-    ' \
-    "abbr-expand-line does not expand a regular abbreviation if it is not at the start of the line"
+        && [[ $reply[rinput] == $reply[routput] ]] \
+        && ((res == 1 ))
+        ' \
+      "abbr-expand-line does not expand a regular abbreviation if it is not at the start of the line"
 
     abbr -g $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line "foo $test_abbr_abbreviation" right_text
     res=$?
     ztr test '[[ $reply[loutput] == "foo $test_abbr_expansion" ]] \
-   && [[ $reply[rinput] == $reply[routput] ]] \
-    && (( res == 0 ))
-    ' \
-    "abbr-expand-line expands a global abbreviation not at the start of the line"
+        && [[ $reply[rinput] == $reply[routput] ]] \
+        && (( res == 0 ))
+        ' \
+      "abbr-expand-line expands a global abbreviation not at the start of the line"
 
     ## BEGIN ~DUPE abbr-expand.ztr.zsh
 
@@ -264,20 +264,20 @@ main() {
     abbr $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line "foo; $test_abbr_abbreviation"
     ztr skip '[[ $reply[loutput] == "foo; $test_abbr_expansion" ]] \
-    && (( res == 0 ))' \
-    "[getting negative result that doesn't match with command line experience] abbr-expand-line expands a regular abbreviation after ;"
+        && (( res == 0 ))' \
+      "[getting negative result that doesn't match with command line experience] abbr-expand-line expands a regular abbreviation after ;"
 
     abbr $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line "foo \& $test_abbr_abbreviation"
     ztr skip '[[ $reply[loutput] == "foo \& $test_abbr_expansion" ]] \
-    && (( res == 0 ))' \
-    "[getting negative result that doesn't match with command line experience] abbr-expand-line expands a regular abbreviation after &"
+        && (( res == 0 ))' \
+      "[getting negative result that doesn't match with command line experience] abbr-expand-line expands a regular abbreviation after &"
 
     abbr $test_abbr_abbreviation=$test_abbr_expansion
     abbr-expand-line "foo | $test_abbr_abbreviation"
     ztr skip '[[ $reply[loutput] == "foo | $test_abbr_expansion" ]] \
-    && (( res == 0 ))' \
-    "[getting negative result that doesn't match with command line experience] abbr-expand-line expands a regular abbreviation after |"
+        && (( res == 0 ))' \
+      "[getting negative result that doesn't match with command line experience] abbr-expand-line expands a regular abbreviation after |"
 
     ABBR_EXPERIMENTAL_COMMAND_POSITION_REGULAR_ABBREVIATIONS=0
 
