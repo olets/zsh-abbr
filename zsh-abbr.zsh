a4e443033f358c808482f794e9c0628ad5335e64
Richard Rutledge <richard@therutledge.com>
2026-04-02 20:47:57 -0700

    UID based tmp dirs

--- zsh-abbr.zsh
+++ zsh-abbr.zsh
@@ -109,11 +109,8 @@ if [[ ${${(k)functions}[(Ie)ABBR_SPLIT_FN]} == 0 ]]; then
 fi
 
 # The directory temp files are stored in
-if [[ -n "${XDG_RUNTIME_DIR}" ]]; then
-  typeset -g _abbr_tmpdir=${ABBR_TMPDIR:-${XDG_RUNTIME_DIR}/zsh-abbr}/
-else
-  typeset -g _abbr_tmpdir=${${ABBR_TMPDIR:-${${TMPDIR:-/tmp}%/}/zsh-abbr-${UID}}}/
-fi
+typeset -g ABBR_TMPDIR=${${ABBR_TMPDIR:+${${ABBR_TMPDIR}%/}/user/$UID}:-${${XDG_RUNTIME_DIR%/}:-${${TMPDIR:-/tmp}%/}/user/$UID}}/zsh-abbr
+typeset -g _abbr_tmpdir=$ABBR_TMPDIR
 
 # The file abbreviations are stored in
 typeset -g ABBR_USER_ABBREVIATIONS_FILE=$ABBR_USER_ABBREVIATIONS_FILE
